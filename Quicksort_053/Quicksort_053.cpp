7639114912d5ed95e5a350aa860f7af79bd691e7
andiniseptia <andiniseptiandri124@gmail.com>
2023-03-31 15:04:48 +0700

    Menambahkan void input

--- Quicksort_053/Quicksort_053.cpp
+++ Quicksort_053/Quicksort_053.cpp
@@ -6,3 +6,16 @@ int arr[20];
 int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of data movement
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMasukkan panjang array adalah 0" << endl;
+	}
+}
